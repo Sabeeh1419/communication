124f175954825cefcc4ae39cd049d4be186e7969
Sabeeh1419 <sabeehhassan29@gmail.com>
2025-03-24 02:35:16 +0500

    file-location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import 'index.css';
+import './index.css';
 
 function App() {
   const [projects, setProjects] = useState([]);
